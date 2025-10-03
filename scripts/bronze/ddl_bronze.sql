c6544be2ecc8ce2b4ea9b3bcc6060aa133e83e77
Temiloluwa Aruna <115124803+Temi-aruna@users.noreply.github.com>
2025-10-03 13:07:39 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -5,7 +5,7 @@ DDL (Data Definition Language) Script: Create Bronze Tables
 Script Purpose: 
   This script creates tables in the 'bronze' schema, dropping existing tables
   if they already exist.
-  Run this script to re-define the DDL structure of 'bronze' Tables
+  You can run this script to re-define the DDL structure of 'bronze' Tables
 =======================================================================================
 */
 
